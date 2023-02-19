309969bc0c49266b4d3d3f60888d253327eda110
koh124 <altkohei212@gmail.com>
2023-02-20 03:24:40 +0900

    add Lambda: It clears cloudfront caches when s3 ObjectCreated

---
+++ serverless/handler.py
@@ -0,0 +1,22 @@
+import boto3
+import json
+import time
+
+cloudfront = boto3.client('cloudfront')
+DISTRIBUTION_ID = 'E1YCQXARB1OVWZ'
+Items = ['/*']
+
+# cloudfrontのキャッシュを削除
+def lambdaHandler(event, context):
+    cloudfront.create_invalidation(DistributionId=DISTRIBUTION_ID, InvalidationBatch={
+        "Paths": {
+            "Quantity": len(Items),
+            "Items": Items
+        },
+        "CallerReference": str(time.time())
+    })
+
+    return {
+        'statusCode': 200,
+        'body': "CloudFront cache successfully deleted"
+    }
